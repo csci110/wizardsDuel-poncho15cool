790d473fe159fcace2a1c0fdd9a50a88a723d92d
Christian Paugh <paughc@dewv.edu>
2018-09-12 16:52:50 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,1 +1,94 @@
+import {game, Sprite} from "./sgc/sgc.js";
 
+game.setBackground("floor.png");
+
+class PlayerWizard extends Sprite {
+    constructor(){
+        super();
+        this.name = "Marcus the Wizard";
+        this.setImage("marcusSheet.png");
+        this.width = 48;
+        this.height = 48;
+        this.x = this.width;
+        this.y = this.height;
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 0, 2);
+        this.defineAnimation("right", 3, 4, 5);
+        this.speedWhenWalking = 100;
+    }
+    handleDownArrowKey() {
+        this.playAnimation("down");
+        this.speed = this.speedWhenWalking;
+        this.angle = 270;
+    }
+    handleUpArrowKey() {
+        this.playAnimation("up");
+        this.speed = this.speedWhenWalking;
+        this.angle = 90;
+    }
+    handleGameLoop() {
+        this.y = Math.max(5, this.y);
+        this.y = Math.min(553, this.y);
+        this.speed = 0;
+        // Keep Marcus in the display area
+    }
+    handleSpacebar() {
+        let spell = new Spell();
+        spell.x = this.x + 48; 
+        // this sets the position of the spell object equal to
+        spell.y = this.y + 48;
+        // the position of any object created from the PlayerWizard class
+        spell.name = "A spell cast by Marcus";
+        spell.setImage("marcusSpellSheet.png");
+        spell.angle = 0;
+        this.playAnimation("right", false); 
+        }
+    }
+    
+let marcus = new PlayerWizard();
+marcus.name = ("Marcus the Wizard");
+
+class Spell extends Sprite {
+    constructor(){
+        super();
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.playAnimation("magic", true);
+        this.defineAnimation("magic", 0, 7);
+    }
+    handleBoundaryContact() {
+    // Delete spell when it leaves display area
+    game.removeSprite(this);
+    }
+    }
+class NonPlayerWizard extends Sprite {
+    constructor(){
+        super();
+        this.name = "The mysterious stranger";
+        this.setImage("strangerSheet.png");
+        this.width = 48;
+        this.height= 48;
+        this.x = game.displayWidth - 2 * this.width
+        this.y = this.height
+        this.angle = 270
+        this.speed = 150
+        this.defineAnimation("down", 6, 8);
+        this.defineAnimation("up", 0, 2);
+    }
+    handleGameLoop() {
+        if (this.y <= 0) {
+      // Upward motion has reached top, so turn down
+      this.y = 0;
+      this.angle = 270;
+      this.playAnimation("down");
+}
+        if (this.y >= game.displayHeight - this.height) {
+      // Downward motion has reached bottom, so turn up
+      this.y = game.displayHeight - this.height;
+      this.angle = 90;
+      this.playAnimation("up");
+}
+    }
+}
+    let stranger = new NonPlayerWizard()
